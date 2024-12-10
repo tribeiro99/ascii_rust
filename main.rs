cdc056f2ab69803939e8feeb1f48030a419381b5
Tiago Ribeiro <161218492+tribeiro99@users.noreply.github.com>
2024-12-10 17:14:30 +0000

    Add files via upload

---
+++ main.rs
@@ -0,0 +1,25 @@
+use image::{GenericImageView};
+use colored::*;
+
+fn get_image(dir: &str, scale: u32) {
+    let img = image::open(dir).expect("Erro ao abrir a imagem");
+    let (width, height) = img.dimensions();
+    println!("Image dimensions: {}x{}", width, height);
+
+    for y in (0..height).step_by((scale * 2) as usize) {
+        for x in (0..width).step_by(scale as usize) {
+            let pix = img.get_pixel(x, y);
+
+            let block = "█".truecolor(pix[0], pix[1], pix[2]);
+
+            print!("{}", block);
+        }
+        println!();    }
+}
+
+fn main() {
+    get_image("pug.png",4); 
+    
+
+}
+
